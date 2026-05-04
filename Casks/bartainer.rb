844c9f5d3a7a811c63e410ff6e24d6a018e6830e
Mert Cobanov <mertcobanov@gmail.com>
2026-05-04 07:38:57 -0700

    Add Bartainer cask

---
+++ Casks/bartainer.rb
@@ -0,0 +1,18 @@
+cask "bartainer" do
+  version "0.2.3"
+  sha256 "2a80ac5ec6df71b2900f22a215ceae4300f41489ceee70b0475feb32d6dc92bb"
+
+  url "https://github.com/cobanov/bartainer/releases/download/v#{version}/Bartainer-#{version}-macos.zip",
+      verified: "github.com/cobanov/bartainer/"
+  name "Bartainer"
+  desc "macOS menu bar for local and remote Docker containers"
+  homepage "https://github.com/cobanov/bartainer"
+
+  depends_on macos: ">= :ventura"
+
+  app "Bartainer.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.cobanov.bartainer.plist",
+  ]
+end
