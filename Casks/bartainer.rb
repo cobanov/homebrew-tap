f4e0ff2752a92d51739a6baa9ba0cb0f2df250e1
Mert Cobanov <mertcobanov@gmail.com>
2026-05-04 07:41:19 -0700

    Use public Bartainer cask asset

--- Casks/bartainer.rb
+++ Casks/bartainer.rb
@@ -2,11 +2,11 @@ cask "bartainer" do
   version "0.2.3"
   sha256 "7dc667b4f7e104274a547ee2cacaac03eee456801771b71fa8d01712cff90527"
 
-  url "https://github.com/cobanov/bartainer/releases/download/v#{version}/Bartainer-#{version}-macos.zip",
-      verified: "github.com/cobanov/bartainer/"
+  url "https://github.com/cobanov/homebrew-tap/releases/download/bartainer-v#{version}/Bartainer-#{version}-macos.zip",
+      verified: "github.com/cobanov/homebrew-tap/"
   name "Bartainer"
   desc "macOS menu bar for local and remote Docker containers"
-  homepage "https://github.com/cobanov/bartainer"
+  homepage "https://github.com/cobanov/homebrew-tap"
 
   depends_on macos: ">= :ventura"
 
